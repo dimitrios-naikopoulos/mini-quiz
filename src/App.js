9ad1473c75c3b47e72631c0941d0e5672fdc510e
dimitrios-naikopoulos <dimitrios.naikopoulos@onthebeach.co.uk>
2019-10-10 09:33:31 +0100

    Transform standard code to be like the examples from the class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Mini Quiz</h1>
-        <p>Answer 10 questions in 60 seconds</p>
-        <button> Go!</button>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    /// Data for 10 questions
+  };
+  render() {
+    const { quotes, randomQuoteIndex } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Mini Quiz</h1>
+          <p>Answer 10 questions in 60 seconds</p>
+          <button> Go!</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
